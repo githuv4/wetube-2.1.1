d7ca0952d2d55685e30715be1ba3eed1ff6a4ad8
githuv4 <sendex485@gmail.com>
2020-06-05 07:25:04 +0900

    3-2.3-setting server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi!");
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+function hanldeListening(req, res) {
+  console.log(`✅ Listening on http://localhost:${PORT}`);
+}
+
+app.listen(PORT, hanldeListening);
