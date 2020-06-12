fdf50725ff7c89b154bf4e928fc71120f641863b
githuv4 <sendex485@gmail.com>
2020-06-12 23:01:13 +0900

    3-2.7~2.12 MVC

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const profile = (req, res) => res.send("Profile");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
+
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("loggout");
+export const search = (req, res) => res.send("search");
---
+++ controllers/videoController.js
@@ -0,0 +1,7 @@
+export const upload = (req, res) => res.send("Upload");
+export const videoDetail = (req, res) => res.send("Video Dtail");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+
+export const home = (req, res) => res.send("Home");
+export const join = (req, res) => res.send("Join");
