2d6a6f91cca915c842a9ec4e878f995f70fc3a2d
githuv4 <sendex485@gmail.com>
2020-06-16 17:21:09 +0900

    +

--- middlewares.js
+++ middlewares.js
@@ -4,8 +4,8 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
-    id: 1,
+    isAuthenticated: false,
+    id: 2155,
   };
   next();
 };
