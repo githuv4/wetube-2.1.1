200cae001610860d12ebdf08bf56d11cc95b9528
githuv4 <sendex485@gmail.com>
2020-06-17 18:59:24 +0300

    3-2.22 Home Controller 2

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
     title: "video 1",
     description: "blablablabal",
     views: 34,
-    videoFile:
+    fileUrl:
       "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
   },
   {
@@ -12,7 +12,7 @@ export const videos = [
     title: "video 2",
     description: "blablablabal",
     views: 134,
-    videoFile:
+    fileUrl:
       "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
   },
   {
@@ -20,7 +20,7 @@ export const videos = [
     title: "video 3",
     description: "blablablabal",
     views: 3,
-    videoFile:
+    fileUrl:
       "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
   },
   {
@@ -28,7 +28,7 @@ export const videos = [
     title: "video 4",
     description: "blablablabal",
     views: 3114,
-    videoFile:
+    fileUrl:
       "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
   },
 ];
