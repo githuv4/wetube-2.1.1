69ecae1c6d72bf8b37c581e98928a8a97dbaead8
githuv4 <sendex485@gmail.com>
2020-06-18 19:57:51 +0300

    4--2.3 settings, ready for server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+function handleListener() {
+  console.log(`✅ Listening on : http://localhost:${PORT}`);
+}
+
+app.get("/", (req, res) => res.send("home1"));
+
+app.listen(PORT, handleListener);
