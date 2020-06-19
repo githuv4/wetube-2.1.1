4dd77042805f3a35fdc93ee41a3b21a7e53942fc
githuv4 <sendex485@gmail.com>
2020-06-19 22:46:38 +0300

    4--2.7 setting middleware

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 3000;
@@ -6,6 +11,12 @@ const PORT = 3000;
 function handleListener() {
   console.log(`✅ Listening on : http://localhost:${PORT}`);
 }
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", (req, res) => res.send("home1"));
 app.get("/profile", (req, res) => res.send("profile"));
