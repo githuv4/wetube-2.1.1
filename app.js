1b5aeee288019cdcd677f282c670710448888afa
githuv4 <sendex485@gmail.com>
2020-06-21 12:08:04 +0300

    4--.~2.17 +

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./localsMi ddleware";
 import userRouter from "./routers/userRouters";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-import { localsMiddleware } from "./localsMiddleware";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
