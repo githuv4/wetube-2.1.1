6f5acc975eaaac29f65a584c2162e489d7c1bb7c
githuv4 <sendex485@gmail.com>
2020-06-21 17:58:51 +0300

    2--3.6 Uploading and Creating a Video : error-file not defined

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -25,10 +25,18 @@ export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 export const postUpload = (req, res) => {
   const {
-    body: { file, title, description },
+    // body: { title, description },
+    file,
+    body,
   } = req;
-  // To Do : Uplaod ad save video
-  res.redirect(routes.videoDetail(1212121));
+  console.log(file, body);
+  // const newVideo = await Video.create({
+  //   fileUrl: path,
+  //   title,
+  //   description,
+  // });
+  // console.log(newVideo);
+  // res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => res.render("videoDetail");
