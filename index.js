caa8f924289d7ffe62ff5c7e3acf0eedd8b00c96
githuv4 <sendex485@gmail.com>
2020-06-21 09:53:17 +0300

    5--2.3 express(server setting), babel, nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hi!");
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => res.send("Home1"));
+
+function handleListening() {
+  console.log(`✅ Listening on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
