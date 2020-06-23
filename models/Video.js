a18ee47054f4345f790fe545c4dd0395f6bc5ba5
githuv4 <sendex485@gmail.com>
2020-06-23 13:36:45 -0300

    3--3.3 Video model

---
+++ models/Video.js
@@ -0,0 +1,25 @@
+import mongoose, { NativeBuffer } from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required",
+  },
+  title: {
+    type: String,
+    required: "Title is required",
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const model = mongoose.model("Video", VideoSchema);
+
+export default model;
