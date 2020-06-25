3ed619920a5fe7cc56c78a80326c3c36029d9443
githuv4 <sendex485@gmail.com>
2020-06-25 17:17:37 -0300

    3--3.4 Comment model

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
 import routes from "../routes";
+import Video from "../models/Video";
+import { permittedCrossDomainPolicies } from "helmet";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const search = (req, res) => {
