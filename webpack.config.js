0d50c678e86d4378dea0a9a247e7421b95832450
githuv4 <sendex485@gmail.com>
2020-07-02 10:11:21 +0900

    2--4.1 Webpack 2 : webpack.config-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [{ test: /\.scss$/, use: "raw-loader" }],
+  },
+  //   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
   output: {
     path: OUTPUT_DIR,
     filename: "[name].js",
