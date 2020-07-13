6396cd485f8c9fa7808028b0be1f0940707b236a
githuv4 <sendex485@gmail.com>
2020-07-14 08:26:48 +0900

    6.5 MongoStore , MIddlewares

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./localsMiddleware";
 
+import "./passport";
+
 const app = express();
+const CookieStore = MongoStore(session);
 
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
@@ -19,6 +26,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,12 +5,13 @@ import {
   changePassword,
   userDetail,
 } from "../controllers/userController";
+import { onlyPrivate } from "../localsMiddleware";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, (req, res) => res.send("User index"));
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
